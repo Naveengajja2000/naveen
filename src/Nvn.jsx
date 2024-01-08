6c9ebe6a5b29b42153b6af4e7a54e326f476649e
Naveengajja2000 <naveengajja290@gmail.com>
2024-01-08 16:38:55 +0530

    nav

---
+++ src/Nvn.jsx
@@ -0,0 +1,13 @@
+import Counter from "./Counter";
+import Todolist from "./Todolist";
+function Nvn(){
+    return(
+        <div className="mybox">
+        <h1>Welcome to ReactJS</h1>
+        <Counter></Counter>
+        <Todolist></Todolist>
+        </div>
+    );
+}
+
+export default Nvn;
